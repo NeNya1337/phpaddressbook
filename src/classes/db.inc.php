ea343b12a3096134bca5f3c10cbb6a485062953a
Mark Lösche <mark.loesche@bechstein.digital>
2022-04-26 12:12:11 +0200

    created ini for dbuser and pass

--- src/classes/db.inc.php
+++ src/classes/db.inc.php
@@ -1,7 +1,10 @@
 <?php
 class Database {
     private $connection;
-    private function dbconnect($dbname, $dbuser = "root", $dbpass = "mddnsm", $dbhost = "localhost"){
+
+    private $config;
+
+    private function dbconnect($dbname, $dbuser, $dbpass, $dbhost = "localhost"){
         $pdo = new PDO("mysql:host=".$dbhost.";dbname=".$dbname, $dbuser, $dbpass);
         return $pdo;
     }
@@ -43,6 +46,7 @@ class Database {
 
     public function __construct()
     {
-        $this->setConnection($this->dbconnect("phpab"));
+        $this->config = parse_ini_file("db.ini");
+        $this->setConnection($this->dbconnect("phpab", $this->config["user"], $this->config["pass"]));
     }
 }
